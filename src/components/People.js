ab23f3c7de91e5326de78e291822f0a16a5d6dc7
stern9 <avram.stern@digitas.com>
2021-03-10 22:13:13 -0600

    Adding pagination, buttons still need work

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,31 @@
+import { useQuery } from "react-query";
+import Person from "./Person";
+
+const fetchCharacters = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
+
 const People = () => {
+  const { data, status } = useQuery("character", fetchCharacters);
+  console.log(data);
+
   return (
     <div>
-      <h2>People</h2>
+      <h2>Characters</h2>
+      {/* <p>{status}</p> */}
+
+      {status === "loading" && <div>Loading data...</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async ({ queryKey }) => {
+  console.log(queryKey[1]);
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${queryKey[1]}`);
+  return res.json();
+};
+
 const Planets = () => {
+  const [page, setPage] = useState(1);
+  const { data, latestData, status } = useQuery(
+    ["planets", page],
+    (page) => fetchPlanets(page),
+    { keepPreviousData: true }
+  );
+
   return (
     <div>
       <h2>Planets</h2>
+
+      {status === "loading" && <div>Loading data</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next page
+          </button>
+          <div>
+            {data.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
